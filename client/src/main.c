8ec047cd092b48464d45492543014ddc13200bba
Anton Laurell <antonlaurell@hotmail.com>
2024-09-21 18:31:13 +0200

    move paddle with mouse

--- client/src/main.c
+++ client/src/main.c
@@ -4,6 +4,26 @@
 #include <emscripten/websocket.h>
 #include "communication.h"
 
+#define PADDLE_SPACING 20
+#define CAMERA_DISTANCE 50
+
+void update(Vector3 *p_pos, Camera3D camera) {
+    Ray mouse = GetMouseRay(GetMousePosition(), camera);
+    float t = (PADDLE_SPACING - mouse.position.z) / mouse.direction.z;
+    p_pos->x = mouse.position.x + mouse.direction.x * t;
+    p_pos->y = mouse.position.y + mouse.direction.y * t;
+}
+
+void draw(Vector3 player_pos, Vector3 opponent_pos, Camera3D camera) {
+    Vector3 paddle_size = { 10, 10, 0 };
+    BeginDrawing();
+    ClearBackground(GRAY);
+    BeginMode3D(camera);
+    DrawCubeWiresV(player_pos, paddle_size, GREEN);
+    DrawCubeWiresV(opponent_pos, paddle_size, GREEN);
+    EndMode3D();
+    EndDrawing();
+}
 
 int main() {
     InitWindow(1, 1, "hlkjlkjj");
@@ -11,28 +31,20 @@ int main() {
             GetMonitorHeight(GetCurrentMonitor()) * 0.6);
     SetTargetFPS(60);
 
-    Vector3 cube_pos = { 0, 0, 0 };
-    int cube_dir = 1;
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10, 10, 10 };
-    camera.target = cube_pos;
-    camera.up = (Vector3){ 0, 1, 0 };
-    camera.fovy = 45;
-    camera.projection = CAMERA_PERSPECTIVE;
-    initSocket();
+    Vector3 player_pos = { 0, 0, PADDLE_SPACING };
+    Vector3 opponent_pos = { 0, 0, -PADDLE_SPACING };
 
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0, 0, CAMERA_DISTANCE };
+    camera.target = (Vector3){ 0, 0, 0 };
+    camera.up = (Vector3){ 0, 1, 0 };
+    camera.fovy = 90;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    initSocket();
     while(!WindowShouldClose()) {
-        cube_pos.y += cube_dir * 0.1f;
-        if (cube_pos.y > 5 || cube_pos.y < -5) {
-            cube_dir *= -1;
-        }
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode3D(camera);
-        DrawCube(cube_pos, 2, 2, 2, GREEN);
-        DrawCubeWires(cube_pos, 2, 2, 2, SKYBLUE);
-        EndMode3D();
-        EndDrawing();
+        update(&player_pos, camera);
+        draw(player_pos, opponent_pos, camera);
     }
     CloseWindow();
     return 0;
